940e66bda81ec7c3261e47eacd668caaf0d0da2e
riverfordhj <riverf_ford@qq.com>
2018-12-20 11:05:13 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+
+// import Cesium from 'cesium/Cesium'
+// import widgets from 'cesium/Widgets/widgets.css'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+Vue.use(ElementUI, {
+  locale
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  // mounted() {
+  //   var viewer = new Cesium.Viewer('cesiumContainer')
+  // },
+  render: h => h(App)
+})
---
+++ src/views/webgis/cesium/cesium.js
@@ -0,0 +1,6 @@
+import Cesium from 'cesium/Cesium';
+import widgets from 'cesium/Widgets/widgets.css';
+
+export const Init = function() {
+  var viewer = new Cesium.Viewer('cesiumContainer')
+}
